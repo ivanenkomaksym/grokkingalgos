c609999d219164293b64b21c7769cd3d3f5ae0b6
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2022-07-31 13:42:54 +0300

    Selection Sort

---
+++ selectionsort.py
@@ -0,0 +1,18 @@
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+# O(n*n) = O(n^2)
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+print(selectionSort([5, 3, 6, 2, 10]))
