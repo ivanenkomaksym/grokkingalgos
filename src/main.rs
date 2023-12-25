fb2bf6a05f5eadb81df1e89c18ec54692a0e5a09
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2023-12-25 11:00:48 +0200

    Use comparible generic instead of fixed type.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-fn find_smallest(v: &Vec<i32>) -> usize{
+fn find_smallest<T>(v: &Vec<T>) -> usize where T: PartialOrd {
     let mut smallest_index = 0 as usize;
-    let mut smallest = v[smallest_index];
+    let mut smallest = &v[smallest_index];
     for i in 1..v.len() {
-        if v[i] < smallest {
+        if v[i] < *smallest {
             smallest_index = i;
-            smallest = v[i];
+            smallest = &v[i];
         }
     }
     smallest_index
 }
 
-fn selection_sort(mut v: Vec<i32>) -> Vec<i32> {
-    let mut sorted = Vec::<i32>::new();
+fn selection_sort<T>(mut v: Vec<T>) -> Vec<T> where T: Copy + PartialOrd {
+    let mut sorted = Vec::<T>::new();
     for _ in 0..v.len() {
         let smallest_index = find_smallest(&v);
         sorted.push(v.remove(smallest_index));
@@ -19,16 +19,16 @@ fn selection_sort(mut v: Vec<i32>) -> Vec<i32> {
     sorted
 }
 
-fn quick_sort(v: Vec<i32>) -> Vec<i32> {
+fn quick_sort<T>(v: Vec<T>) -> Vec<T> where T: Copy + PartialOrd {
     if v.len() < 2 {
         return v;
     }
 
-    let pivot = v[0];
-    let mut less = Vec::<i32>::new();
-    let mut greater = Vec::<i32>::new();
+    let pivot = &v[0];
+    let mut less = Vec::<T>::new();
+    let mut greater = Vec::<T>::new();
     for i in &v[1..] {
-        if i <= &pivot {
+        if i <= pivot {
             less.push(*i);
         }
         else {
@@ -37,7 +37,7 @@ fn quick_sort(v: Vec<i32>) -> Vec<i32> {
     }
     
     let mut result = quick_sort(less); 
-    result.push(pivot);
+    result.push(pivot.clone());
     result.extend(quick_sort(greater));
 
     result
@@ -45,7 +45,7 @@ fn quick_sort(v: Vec<i32>) -> Vec<i32> {
 
 fn main() {
     println!("===Selection Sort===");
-    let vec = vec![5, 2, 3, 4, 9];
+    let vec = vec![5.4, 5.2, 5.3, 5.4, 9.0];
     let sorted = selection_sort(vec);
 
     for x in &sorted {
@@ -53,7 +53,7 @@ fn main() {
     }
     
     println!("===Quick Sort===");
-    let vec = vec![5, 2, 3, 4, 9];
+    let vec = vec![5.4, 5.2, 5.3, 5.4, 9.0];
     let sorted = quick_sort(vec);
 
     for x in &sorted {
